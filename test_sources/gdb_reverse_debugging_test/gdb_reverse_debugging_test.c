0846e95af77605c29e8644e93801c23fbe44dc00
Andreas Reiter <andreas.reiter@student.tugraz.at>
2011-06-05 14:06:07 +0200

    Modified testfiles

--- test_sources/gdb_reverse_debugging_test/gdb_reverse_debugging_test.c
+++ test_sources/gdb_reverse_debugging_test/gdb_reverse_debugging_test.c
@@ -2,25 +2,31 @@
 
 int xyz;
 
-int bar ()
+int bar (int bar_var, int bar_var2)
 {
-  xyz = 2; /* break in bar */
+  long long local_bar_val;
+  local_bar_val = 123;
+  xyz = bar_var; /* break in bar */
   return 1;
 }
 
 int foo ()
 {
+  int foo_var;
+  foo_var = 1;
   xyz = 1; /* break in foo */
-  return bar ();
+  return bar (456,0 );
 }
 
 int main ()
 {
-//  char* text = (char*) malloc(10000);
-//  int i;
-//  for(i=0; i< 150000; i++)
-//    text[i] = 'a';
-
+int abc;
+abc = 1;
+/*char* text = (char*) malloc(10000);
+int i;
+for(i=0; i< 150000; i++)
+    text[i] = 'a';
+*/
   xyz = 0;      /* break in main */
   puts("bubu");
   foo ();
