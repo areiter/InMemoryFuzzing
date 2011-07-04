c7fc1f56647d51c49e8911be25b43ae648b47c1d
Andreas Reiter <andreas.reiter@student.tugraz.at>
2011-07-04 16:56:46 +0200

    ...

--- test_sources/gdb_reverse_debugging_test/.#gdb_reverse_debugging_test.c
+++
@@ -1,1 +0,0 @@
-andi@andi-netbook.1831:1301752774
--- test_sources/gdb_reverse_debugging_test/gdb_reverse_debugging_test.c
+++ test_sources/gdb_reverse_debugging_test/gdb_reverse_debugging_test.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int xyz;
 
-int bar (int bar_var, int bar_var2)
+int bar (char* mydata)
 {
-  long long local_bar_val;
-  local_bar_val = 123;
-  xyz = bar_var; /* break in bar */
+  printf("%s \n", mydata);
+  fflush(stdout);
   return 1;
 }
 
@@ -15,18 +15,16 @@ int foo ()
   int foo_var;
   foo_var = 1;
   xyz = 1; /* break in foo */
-  return bar (456,0 );
+  return bar ("ORIGINAL_DATA" );
 }
 
 int main ()
 {
 int abc;
 abc = 1;
-/*char* text = (char*) malloc(10000);
-int i;
-for(i=0; i< 150000; i++)
-    text[i] = 'a';
-*/
+//char* text = "hello world";
+//*text = 'H';
+//  getchar();
   xyz = 0;      /* break in main */
   puts("bubu");
   foo ();
