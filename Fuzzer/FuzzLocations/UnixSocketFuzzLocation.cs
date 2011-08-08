f53790cc4dea206ca5196374a6036385645401ce
Andreas Reiter <andreas.reiter@student.tugraz.at>
2011-08-08 17:14:48 +0200

    Added UnixSocketFuzzLocation

---
+++ Fuzzer/FuzzLocations/UnixSocketFuzzLocation.cs
@@ -0,0 +1,66 @@
+// UnixSocketFuzzLocation.cs
+//  
+//  Author:
+//       Andreas Reiter <andreas.reiter@student.tugraz.at>
+// 
+//  Copyright 2011  Andreas Reiter
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+using System;
+using System.Xml;
+using Fuzzer.TargetConnectors;
+using Iaik.Utils.Net;
+using Iaik.Utils;
+namespace Fuzzer.FuzzLocations
+{
+	
+	public class UnixSocketFuzzLocation : BaseFuzzLocation
+	{
+		private UnixSocketConnection _socket;
+			
+		#region implemented abstract members of Fuzzer.FuzzLocations.BaseFuzzLocation
+		protected override bool SupportsStopCondition 
+		{
+			get { return true; }
+		}
+		
+		
+		protected override bool SupportsDataGen 
+		{
+			get { return true; }
+		}
+		
+		
+		protected override bool SupportsTrigger 
+		{
+			get { return true; }
+		}
+		
+		#endregion
+		
+		public override void Init (XmlElement fuzzLocationRoot, ITargetConnector connector)
+		{
+			base.Init (fuzzLocationRoot, connector);
+			
+			_socket = new UnixSocketConnection (XmlHelper.ReadString (fuzzLocationRoot, "SocketPath"));
+			_socket.Connect ();
+		}
+		
+		public override void Run (FuzzController ctrl)
+		{
+			base.Run (ctrl);
+		}
+		
+	}
+}
+
